7d09f980ec4eb03f3f1a47e5c3c0d53e91721636
Iris Lim <irislim@Iriss-MacBook-Pro.local>
2022-11-11 01:03:14 +0800

    Manual Testing for BMI Calculator

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -1,7 +1,19 @@
 function calculateBMI(height, weight) {
     let heightMeter = height * 0.01
-    let result = weight / (Math.pow(heightMeter, 2));
-    return result;
+    let result1 = weight / (Math.pow(heightMeter, 2));
+    return Number(result1.toFixed(1));
 }
 
-console.log(calculateBMI(157, 50)); // (kg, cm)
+// console.log(calculateBMI(157, 50)); // (kg, cm)
+
+function testCalculator() {
+    const result = calculateBMI(157, 50);
+    const expected = 20.3;
+
+    console.assert(
+        result === expected,
+        `The result ${result} doesn't match the expected ${expected}`
+    );
+}
+
+testCalculator();
